6070b5c8723313bda6b07ffbe9150fcd589913a9
Akshay Chandrashekaran <akshayc@cmu.edu>
2016-09-05 22:37:32 -0700

    feat: Copied Google Maps Draggable Polygon demo
    Using environment variables from heroku

--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 def index(request):
     return HttpResponse("Hello, world. You're at the survey index.")
@@ -7,4 +8,6 @@ def index(request):
 
 def demo_map(request):
     # View code here...
-    return render(request, 'survey/demo_map.html')
+    return render(request,
+                  'survey/demo_map.html',
+                  {'api_key' : os.environ["GOOGLE_MAPS_API_KEY"]})
