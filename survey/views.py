1dbc8d3b8839b2d212e0a995883ce098f7cc02d0
Akshay Chandrashekaran <akshayc@cmu.edu>
2016-09-05 21:36:32 -0700

    feat: added template and javascript for demo maps

--- survey/views.py
+++ survey/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the survey index.")
+
+
+def demo_map(request):
+    # View code here...
+    return render(request, 'survey/demo_map.html')
