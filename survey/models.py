62398c499bd38e7f1c31174956d37183a2a6b937
Erika Menezes <menezes.erika90@gmail.com>
2016-09-14 20:52:36 -0700

    Added Question, TMCQ, ProjectResponse, QuestionResponse, TMCQResponse

--- survey/models.py
+++ survey/models.py
@@ -12,5 +12,34 @@ class Project(models.Model):
 	name = models.CharField(max_length = 100)
 	feedback_goals = models.ManyToManyField(FeedbackGoal)
 
+class Question(models.Model):
+	feedback_goal = models.ForeignKey('FeedbackGoal', on_delete=models.CASCADE)
+	question_text = models.CharField(max_length = 200)
 
-    
+class TMCQ(Question):
+	option1 = models.CharField(max_length = 30)
+	option2 = models.CharField(max_length = 30)
+	option3 = models.CharField(max_length = 30)
+	option4 = models.CharField(max_length = 30)
+	option5 = models.CharField(max_length = 30)
+
+class ProjectResponse(models.Model):
+	project = models.ForeignKey(
+			'Project',
+			on_delete = models.CASCADE,
+		)
+	creation_time = models.DateTimeField(auto_now_add=True)
+	#ip_address = models.CharField(max_length = 30)
+
+class QuestionResponse(models.Model):
+	project_response = models.ForeignKey(
+			'ProjectResponse',
+			on_delete = models.CASCADE,
+		)
+	question = models.ForeignKey(
+			'Question',
+			on_delete = models.CASCADE,
+		)
+
+class TMCQResponse(QuestionResponse):
+	option_index = models.IntegerField()
