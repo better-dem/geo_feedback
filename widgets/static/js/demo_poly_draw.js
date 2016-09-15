c3b23ceaa5d14b9767fbd049062f580df0445243
Akshay Chandrashekaran <akshayc@cmu.edu>
2016-09-14 19:36:23 -0700

    fix: changed a css tag to get the widget working again

--- widgets/static/js/demo_poly_draw.js
+++ widgets/static/js/demo_poly_draw.js
@@ -5,17 +5,6 @@
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // Display the coordinates below the map
-function getPolygonCoords() {
-	var len = my_polygon.getPath().getLength();
-	var htmlStr = "";
-	for (var i = 0; i < len; i++) {
-		htmlStr += "new google.maps.LatLng(" + 
-			my_polygon.getPath().getAt(i).toUrlValue(5) + 
-			"), ";
-	}
-	console.log(htmlStr);
-	//document.getElementById('info').innerHtml = htmlStr;
-}
 
 function initMap() {
 	// Initialize map to certain coordinates
@@ -53,5 +42,14 @@ function initMap() {
 
 }
 
-
-
+function getPolygonCoords() {
+	var len = my_polygon.getPath().getLength();
+	var htmlStr = "";
+	for (var i = 0; i < len; i++) {
+		htmlStr += "new google.maps.LatLng(" + 
+			my_polygon.getPath().getAt(i).toUrlValue(5) + 
+			"), ";
+	}
+	console.log(htmlStr);
+	//document.getElementById('info').innerHtml = htmlStr;
+}
