557c44705894f653a11147d1484eb5d9caebeea7
Akshay Chandrashekaran <akshayc@cmu.edu>
2016-09-17 18:06:21 -0700

    changing function order

--- widgets/static/js/demo_poly_draw.js
+++ widgets/static/js/demo_poly_draw.js
@@ -4,7 +4,17 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// Display the coordinates below the map
+function getPolygonCoords() {
+	var len = my_polygon.getPath().getLength();
+	var htmlStr = "";
+	for (var i = 0; i < len; i++) {
+		htmlStr += "new google.maps.LatLng(" + 
+			my_polygon.getPath().getAt(i).toUrlValue(5) + 
+			"), ";
+	}
+	console.log(htmlStr);
+	//document.getElementById('info').innerHtml = htmlStr;
+}
 
 function initMap() {
 	// Initialize map to certain coordinates
@@ -42,14 +52,3 @@ function initMap() {
 
 }
 
-function getPolygonCoords() {
-	var len = my_polygon.getPath().getLength();
-	var htmlStr = "";
-	for (var i = 0; i < len; i++) {
-		htmlStr += "new google.maps.LatLng(" + 
-			my_polygon.getPath().getAt(i).toUrlValue(5) + 
-			"), ";
-	}
-	console.log(htmlStr);
-	//document.getElementById('info').innerHtml = htmlStr;
-}
