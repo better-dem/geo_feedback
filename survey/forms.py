da7b2cbabff62ea48c668575c1d92cb482742d24
David Cohen <david.cohen@west.cmu.edu>
2016-10-13 23:03:19 -0700

    added project results view, which shows a table of responses for each question

--- survey/forms.py
+++ survey/forms.py
@@ -18,25 +18,24 @@ class ProjectResponseForm(forms.Form):
     def __init__(self, project, *args, **kwargs):
         super(ProjectResponseForm, self).__init__(*args, **kwargs)
         ans = project.name+ ":"
-        for goal in project.feedback_goals.all():
-            for question in Question.objects.filter(feedback_goal = goal):
-                try:
-                    tmcq = question.tmcq
-                    label = question.question_text
-                    var_name = "field_prf_" + str(question.id)
-                    choices = []
+        for question in project.get_questions():
+            try:
+                tmcq = question.tmcq
+                label = question.question_text
+                var_name = "field_prf_" + str(question.id)
+                choices = []
 
-                    if not tmcq.option1 == "":
-                        choices.append(("1", tmcq.option1))
-                    if not tmcq.option2 == "":
-                        choices.append(("2", tmcq.option2))
-                    if not tmcq.option3 == "":
-                        choices.append(("3", tmcq.option3))
-                    if not tmcq.option4 == "":
-                        choices.append(("4", tmcq.option4))
-                    if not tmcq.option5 == "":
-                        choices.append(("5", tmcq.option5))
-                    
-                    self.fields[var_name] = forms.ChoiceField(label = label, required=False, choices = choices )
-                except:
-                    raise Exception("Invalid question type. Only TMCQ supported")
+                if not tmcq.option1 == "":
+                    choices.append(("1", tmcq.option1))
+                if not tmcq.option2 == "":
+                    choices.append(("2", tmcq.option2))
+                if not tmcq.option3 == "":
+                    choices.append(("3", tmcq.option3))
+                if not tmcq.option4 == "":
+                    choices.append(("4", tmcq.option4))
+                if not tmcq.option5 == "":
+                    choices.append(("5", tmcq.option5))
+
+                self.fields[var_name] = forms.ChoiceField(label = label, required=False, choices = choices )
+            except:
+                raise Exception("Invalid question type. Only TMCQ supported")
